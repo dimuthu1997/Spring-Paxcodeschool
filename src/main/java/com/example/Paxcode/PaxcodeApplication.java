210d5dbb84e0a5d3b8551ec1ea9de8b397251d5e
Dimuthu Dilshan <dimuthud2017@gmail.com>
2022-07-27 22:14:07 +0530

    First Commit

---
+++ src/main/java/com/example/Paxcode/PaxcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Paxcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaxcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaxcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Paxcode/PaxcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Paxcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaxcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
